14c9aa9b66dcbd1ebf1dfdad94b5052aea1af3af
irinainina <irbris@mail.ru>
2022-03-24 15:23:13 +0200

    feat: update demo 14

--- day4/threejs14/index.js
+++ day4/threejs14/index.js
@@ -1,75 +1,88 @@
 // https://github.com/edwinwebb/three-seed
 //импорт библиотеки three.js
-import * as THREE from "three";
-import { GLTFLoader } from "https://unpkg.com/three@0.138.3/examples/jsm/loaders/GLTFLoader";
+import * as THREE from 'three';
+import { OrbitControls } from './js/OrbitControls.js';
+import { GLTFLoader } from './js/GLTFLoader.js';
 
 const width = window.innerWidth;
 const height = window.innerHeight;
 const aspect = width / height;
 
 //создание канваса, начальные настройки сцены, камеры и т.д.
-  const scene = new THREE.Scene();
+const scene = new THREE.Scene();
 
-  const camera = new THREE.PerspectiveCamera(75, aspect, .1, 1000);
-  camera.position.z = 21;
+const camera = new THREE.PerspectiveCamera(75, aspect, 0.1, 1000);
+camera.position.z = 10;
 
-  const renderer = new THREE.WebGLRenderer({antialias: true, alpha: true});
-  renderer.setSize(width, height);
-  document.body.append(renderer.domElement);
+const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+renderer.setSize(width, height);
+document.body.append(renderer.domElement);
 
-  const point = new THREE.PointLight(0xFFFFFF, 1, 10, 1);
-  const dir = new THREE.SpotLight(0xFFFFFF, 0.8, 7, 0.8, 1, 1);
-  const ambi = new THREE.AmbientLight( 0x404040 , 0.66);
-  const hemi = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1.15 )
+const point = new THREE.PointLight(0xffffff, 1, 10, 1);
+const dir = new THREE.SpotLight(0xffffff, 0.8, 7, 0.8, 1, 1);
+const ambi = new THREE.AmbientLight(0x404040, 0.66);
+const hemi = new THREE.HemisphereLight(0xffffbb, 0x080820, 1.15);
 
-  dir.position.set(5, 1, 2);
-  dir.target.position.set(0,0,0);
+dir.position.set(5, 1, 2);
+dir.target.position.set(0, 0, 0);
 
-  point.position.set(0, 1, 5);
-  point.intensity = 0.5;
+point.position.set(0, 1, 5);
+point.intensity = 0.5;
 
-  scene.add(point, ambi, hemi, dir);
-  
+scene.add(point, ambi, hemi, dir);
 
+// OrbitControls
+const renderCalls = [];
+function render() {
+  requestAnimationFrame(render);
+  renderCalls.forEach((callback) => {
+    callback();
+  });
+}
+render();
 
-//загрузка модели цветка и установление её настроек
-  const loaderFlower = new GLTFLoader();
+const controls = new OrbitControls(camera, renderer.domElement);
 
-  loaderFlower.load( './model/Flower.glb', function ( gltf ) {
-  gltf.scene.position.set(0, 0, 15);
-  gltf.scene.scale.set(0.8, 0.8, 0.8);
+controls.rotateSpeed = 0.3;
+controls.zoomSpeed = 0.9;
 
-  gltf.scene.rotation.y = 1.5;
-  gltf.scene.rotation.x = 0.4;
+controls.minDistance = 6;
+controls.maxDistance = 22;
 
-  scene.add( gltf.scene );
+controls.minPolarAngle = 0; // radians
+controls.maxPolarAngle = Math.PI / 2; // radians
+
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
+
+renderCalls.push(function () {
+  controls.update();
+});
+
+//загрузка модели цветка
+const loaderFlower = new GLTFLoader();
+loaderFlower.load('./model/Flower.glb', function (gltf) {
+  scene.add(gltf.scene);
 
   function render() {
-    gltf.scene.rotation.y += 0.002;
+    gltf.scene.rotation.y += 0.005;
     renderer.render(scene, camera);
     requestAnimationFrame(render);
   }
   render();
 });
 
-//загрузка модели земли и установление её настроек
+//загрузка модели земли
 const loaderLand = new GLTFLoader();
+loaderLand.load('./model/Land.glb', function (gltf) {
+  scene.add(gltf.scene);
 
-loaderLand.load( './model/Land.glb', function ( gltf ) {
-gltf.scene.position.set(0, 0, 15);
-gltf.scene.scale.set(0.8, 0.8, 0.8);
-
-gltf.scene.rotation.y = 1.5;
-gltf.scene.rotation.x = 0.4;
-
-scene.add( gltf.scene );
-
-function render() {
-  gltf.scene.rotation.y += 0.002;
-  renderer.render(scene, camera);
-  requestAnimationFrame(render);
-}
-render();
+  function render() {
+    gltf.scene.rotation.y += 0.005;
+    renderer.render(scene, camera);
+    requestAnimationFrame(render);
+  }
+  render();
 });
 
 function onWindowResize() {
